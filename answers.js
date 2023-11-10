bcdb1bfdbdbb51553f894f9b12479f14099a2d27
mackwick <mackenziewicker@gmail.com>
2023-11-10 16:19:23 -0500

    Find the Median answered

--- answers.js
+++ answers.js
@@ -172,8 +172,12 @@ for (let i = 1; i < 21; i++) {
 ////////////////////////////////
 //  Find the Median
 ////////////////////////////////
+//Find the median number in the following nums array, then console.log that number. - Hint if you check the length of the array / 2, you might get not get a whole number. In which case, look into Math.floor( // something )
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
 
-
+nums.sort()
+console.log(nums.length)
+console.log(nums[13]) // It's 15
 
 //*************************** */
 // Hungry for More?
