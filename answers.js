1e4a9e5172af849a6fa78dfdd9b77df33baa3f0e
mackwick <mackenziewicker@gmail.com>
2023-11-10 15:58:12 -0500

    Where is Waldo answered and tbh I am very proud

--- answers.js
+++ answers.js
@@ -132,7 +132,24 @@ console.log(halfFavMovies)
 ////////////////////////////////
 // Where is Waldo
 ////////////////////////////////
+const whereIsWaldo = [
+                    ["Timmy", "Frank"], 
+                    "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", 
+                            ["Baked Goods", "Waldo"]]
+                ];
 
+// Remove Eggbert (hint look at the slice/splice method(s))
+whereIsWaldo.splice(1, 1)
+console.log(whereIsWaldo)
+
+// Change "Neff" to "No One"
+whereIsWaldo[1].splice(2, 1, "No One")
+console.log(whereIsWaldo)
+
+// Access and console.log "Waldo"
+console.log(whereIsWaldo[2][1][1])
 
 ////////////////////////////////
 //  Excited Kitten
